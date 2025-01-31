d49f294beaad5345e09d0466760c40fa6152068a
shuremali02 <shuremsyed41@gmail.com>
2025-01-31 09:39:17 -0800

    final changes updated

--- app/Component/card.tsx
+++ app/Component/card.tsx
@@ -7,7 +7,7 @@ export default function NftCard () {
     return (
       <div>
         <div className="flex items-center justify-center  bg-gray-100">
-          <h1 className="md:text-7xl p-4 text-3xl sm:text-5xl font-bold hover:animate-pulse">TOP NFT's</h1>
+          <h1 className="md:text-7xl p-4 text-3xl sm:text-5xl font-bold hover:animate-pulse">TOP NFT&#39;s</h1>
         </div>
       <div className="grid grid-cols sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
         {nftData.map((nft, index) => (
